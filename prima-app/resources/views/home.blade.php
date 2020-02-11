2f64c60fc592d165a2223cdb4c4274313d0a83ec
lucapu88 <caputoluca88@gmail.com>
2020-02-11 16:35:09 +0100

    finito parte del layout homepage

--- prima-app/resources/views/home.blade.php
+++ prima-app/resources/views/home.blade.php
@@ -2,10 +2,16 @@
 @section('content')
   <div class="jumbo">
     <div class="jumboLeft">
-
+      <p>DIVENTA <strong>SVILUPPATORE WEB</strong></p>
+      <h3>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</h3>
+      <ul>
+        <li>6 mesi di corso intensivo online in diretta</li>
+        <li>Nessuna competenza di programmazione richiesta</li>
+        <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+      </ul>
     </div>
     <div class="jumboRight">
-
+      <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
     </div>
   </div>
 @endsection
