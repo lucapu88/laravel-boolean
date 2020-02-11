c1dda4cfd645e0eb634084e7e7d99e2b0074d1aa
lucapu88 <caputoluca88@gmail.com>
2020-02-11 12:06:35 +0100

    inizio

---
+++ prima-app/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://www.boolean.careers/images/favicon/favicon.ico" />
+    <title>Il corso per diventare web developer</title>
+  </head>
+  <body>
+
+  </body>
+</html>
