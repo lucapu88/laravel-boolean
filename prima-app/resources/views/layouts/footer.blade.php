7efe1f33d2915d4493fa23004dc66032f4e015f9
lucapu88 <caputoluca88@gmail.com>
2020-02-11 16:19:47 +0100

    finito footer e parte del layout

--- prima-app/resources/views/layouts/footer.blade.php
+++ prima-app/resources/views/layouts/footer.blade.php
@@ -1,2 +1,22 @@
 <footer>
+  <div class="footerContainer">
+    <div class="top">
+      <div class="left">
+        <img src="https://www.boolean.careers/images/common/logo-white.png" alt="">
+        <p>Sede Operativa</p>
+        <p>Via Carducci 12 - 20123 Milano</p>
+        <p>Tel: 02-36599712</p>
+      </div>
+      <div class="right">
+        <a href="#">Lezione gratuita</a>
+        <a href="#">Domande frequenti</a>
+        <a href="#">Per le aziende</a>
+        <a href="#">Privacy policy</a>
+        <a href="#">Lavora con noi</a>
+      </div>
+    </div>
+    <div class="bottom">
+      <p>Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967</p>
+    </div>
+  </div>
 </footer>
--- prima-app/resources/views/layouts/main.blade.php
+++ prima-app/resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="https://www.boolean.careers/images/favicon/favicon.ico" />
     <title>Il corso per diventare web developer</title>
+    <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('/css/app.css')}}">
   </head>
   <body>
